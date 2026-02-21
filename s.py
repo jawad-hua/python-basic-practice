6a84aed49952825792e9d2db458ac4467eb3aa00
jawad-hua <jawadmjawad06@gmail.com>
2026-02-21 20:13:41 +0500

    basic python

---
+++ s.py
@@ -0,0 +1,164 @@
+# print("hello:my name is m jawad")
+
+# print("how are you?")
+
+# print("my name is M.Jawad and my friend name is Ali")
+# name="jawad"
+# age=21
+# print(name,age) 
+
+# a=456
+# b=4567
+# c=a+b
+# c=a/b
+# c=a*b
+# c=a-b
+# print(type(c))
+
+# qua=13
+# if qua>12:
+#     print("you are eligible of this course")
+# else:
+#     print("you are not eligible")
+
+# x=23
+# y=45
+# print("the value is",x, "+",y , "=",x+y )
+# print("the value is",x, "-",y , "=",x-y )
+# print("the value is",x, "*",y , "=",x*y )
+# print("the value is",x, "/",y , "=",x/y )
+
+
+# n=18
+# if n>=18:
+#     print("you can drive")
+# else:
+#     print("you can not drive")
+
+
+# price=500
+# budget=380
+# if price<=budget:
+#     print("alexa: Please add 1KG of apple. ")
+# else:
+#     print("alexa: Do not add apple")
+
+
+# price=50
+# budget=380
+# if (budget-price>50):
+#     print("alexa: Please add 1KG of apple. ")
+# else:
+#     print("alexa: Do not add apple")
+
+
+
+# number=-23
+# if number>0:
+#     print("Number is positive")
+# elif number==0:
+#     print("Number is Zero")
+# else:
+#     print("Number is Negative")
+
+
+# A=300
+# B=400
+# if A>B:
+#     print(A)
+# else:
+#     print(B)
+
+
+# a=12
+# if a%2==0:
+#     print("number is even")
+# else:
+#     print("number is odd")
+
+# k=3
+# print(k,"x 1 =", k*1)
+# print(k,"x 2 =", k*2)
+# print(k,"x 3 =", k*3)
+# print(k,"x 4 =", k*4)
+# print(k,"x 5 =", k*5)
+# print(k,"x 6 =", k*6)
+# print(k,"x 7 =", k*7)
+# print(k,"x 8 =", k*8)
+# print(k,"x 9 =", k*9)
+# print(k,"x 10 =", k*10)
+
+
+# x=2
+# y=str(x)
+# print(type(y))
+
+# x, y, z= 10, 20, 30
+# print(x, y, z)
+
+# name="jawad"
+# print(type(name))
+
+# name=input("enter your name:")
+# age=int(input("enter you age:"))
+# print("hello", name)
+# print(age+1)
+
+# nane="jawad"
+# age=18
+
+# print(f"your name is {nane} and you are  {age} years old")
+
+# x,y=map(int, input("number:").split())
+# print(x+y)
+
+
+# mark=int(input("enter your marks:"))
+# if (mark>80 and mark<100):
+#     print("A grade")
+# elif (mark>60 and mark<80):
+#     print("B Grade")
+# elif (mark>40 and mark<60):
+#     print("C Grade")
+# else:
+#     print("fail")
+
+
+# age=int(input("enter your age please:"))
+# citizen=True
+# if age>=18:
+#     if citizen:
+#         print("eligible to vote")
+#     else:
+#         print("not a citizen cannnot vote")
+# else:
+#     print("too young to vote")
+
+# for i in range(1,4000):
+#     print(i)
+
+# f=["apple","mango","cherry"]
+# for i in f:
+#     print(i)
+
+# name=input()
+# edu=input()
+# cgp=int(input())
+# uni=input() 
+# print(f"My name is {name} and i am in {edu} with {cgp}GPA in {uni}")
+
+name="Muhammad Jawad"
+age=20
+Degree="BS IT"
+Semester=6
+gpa=3.4
+print(f"my name is {name} and i am {age} years old. i am studying {Degree} in {Semester} semester.my gpa is {gpa}")
+subject5th=["database","information security","theory of autometa", "financial accounting","ai","virtual system and services"]
+grades=["A","B","A","C","B","B"]
+print("the result of 5th semester with grades")
+for i in  range(len(subject5th)):
+    print(subject5th[i],":",grades[i])
+subjects6th=["web tecnologies","Machine Learning","IT Infrastructure","Introduction to Management","enterprise System","Network security","civic and cumininty management"]
+print("this semester", len(subjects6th),"subject which are given below")
+for subject in subjects6th:
+    print(subject)
